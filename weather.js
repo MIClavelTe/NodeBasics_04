0f3ef09da14bc570a288b9da24f374e5db9e58c8
MIClavelTe <miclavelte@gmail.com>
2018-07-23 17:58:19 -0700

    Add print functions

--- weather.js
+++ weather.js
@@ -1,2 +1,18 @@
 const https = require('https');
+const api = require('./api.json');
 
+function printMessage(place, temp, summary) {
+    const title = `Forecast in ${place}:`;
+    const desc = `Summary: ${summary}`;
+    const weather = `Temperature: ${temp}˚F`
+    console.log(title);
+    console.log(desc);
+    console.log(weather);
+    console.log("");
+  }
+  
+  function printError(message) {
+    console.error(message);
+  }
+  
+  
